a1739c5abe2a2ed8f1ea59bfe4f476c80953c86c
Satya Karki <spacegrlsatya@gmail.com>
2022-10-20 19:54:48 +0800

    Add project files.

---
+++ CalculationContract.cs
@@ -0,0 +1,50 @@
+﻿using Stratis.SmartContracts;
+using System;
+
+public class CalculationContract : SmartContract
+{
+    // setting firstvalue and secondvalue while deploying the contract 
+    public CalculationContract(ISmartContractState smartContractState, Int32 firstValue, Int32 secondVal)
+   : base(smartContractState)
+    {
+        this.Firstvalue = firstValue;
+        this.SecondValue = secondVal;
+    }
+
+    public Int32 Firstvalue
+    {
+        get => PersistentState.GetInt32(nameof(Firstvalue));
+        private set => PersistentState.SetInt32(nameof(Firstvalue), value);
+    }
+
+    private Int32 SecondValue
+    {
+        get
+        {
+            return this.PersistentState.GetInt32("SecondValue");
+        }
+        set
+        {
+            this.PersistentState.SetInt32("SecondValue", value);
+        }
+    }
+
+    public long GetSum()
+    {
+        long sum = (Firstvalue + SecondValue);
+        return sum;
+    }
+
+    public long GetMultiply()
+    {
+        long mul = (Firstvalue * SecondValue);
+        return mul;
+    }
+
+    public long GetDivision()
+    {
+        long div = (SecondValue / Firstvalue);
+        return div;
+    }
+}
+
---
+++ MyContract.cs
@@ -0,0 +1,48 @@
+﻿using Stratis.SmartContracts;
+using System;
+
+public class MyContract : SmartContract
+{
+    public MyContract(ISmartContractState smartContractState)
+    : base(smartContractState)
+    {
+        this.Firstvalue = 5;
+        this.SecondValue = 10;
+    }
+
+    public Int32 Firstvalue
+    {
+        get => PersistentState.GetInt32(nameof(Firstvalue));
+        private set => PersistentState.SetInt32(nameof(Firstvalue), value);
+    }
+
+    private Int32 SecondValue
+    {
+        get
+        {
+            return this.PersistentState.GetInt32("SecondValue");
+        }
+        set
+        {
+            this.PersistentState.SetInt32("SecondValue", value);
+        }
+    }
+    
+    public long GetSum()
+    {
+        long sum = (Firstvalue + SecondValue);
+        return sum;
+    }
+
+    public long GetMultiply()
+    {
+        long mul = (Firstvalue * SecondValue);
+        return mul;
+    }
+
+    public long GetDivision()
+    {
+        long div = (SecondValue / Firstvalue);
+        return div;
+    }
+}
